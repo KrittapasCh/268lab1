1f9c4af38bd2a705c63d409e8ceda49cb6e82c46
KrittapasCh <s6804062612293@email.kmutnb.ac.th>
2025-12-09 13:59:48 +0700

    Add files via upload

---
+++ sale.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	float price1,price2,price3,discount1,discount2,discount3;
+	scanf("%f",&price1);
+	discount1 = price1*0.95;
+	printf("%f\n",discount1);
+	
+	scanf("%f",&price2);
+	discount2 = (price1+price2)*0.85;
+	printf("%f\n",discount2);
+	
+	scanf("%f",&price3);
+	discount3 = (price1+price2+price3)*0.70;
+	printf("%f\n",discount3);
+	return 0;
+}
